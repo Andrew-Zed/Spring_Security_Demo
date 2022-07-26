b6c5a697af99794e9728d4454d18ee3b7b5776c7
Andrew-Zed <andrew.awadyke@gmail.com>
2022-07-27 00:46:34 +0100

    first commit

---
+++ src/test/java/com/andrew/spring_security_demo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.spring_security_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
