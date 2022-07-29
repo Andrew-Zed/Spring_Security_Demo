a0ed056ff88068963ed5db0c6caf6878813af06a
Andrew-Zed <andrew.awadyke@gmail.com>
2022-07-29 01:05:16 +0100

    role based permission added

--- src/main/java/com/andrew/spring_security_demo/security/ApplicationUserRole.java
+++ src/main/java/com/andrew/spring_security_demo/security/ApplicationUserRole.java
@@ -1,8 +1,11 @@
 package com.andrew.spring_security_demo.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.andrew.spring_security_demo.security.ApplicationUserPermission.*;
 
@@ -16,5 +19,17 @@ public enum ApplicationUserRole {
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()) );
+        return permissions;
     }
 }
